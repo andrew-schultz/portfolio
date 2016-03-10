fda199977a688c3bcac70f36b67129e28b26fabd
Andrew Schultz <as173171@gmail.com>
2016-03-09 20:52:10 -0500

    changed around the style a lot, removed most of the about me section, reformated the projects section entirely

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-	$('.aboutImg').hover(function(){
+	$('.workBubble').hover(function(){
 		$(this).children().fadeIn(250).stop;
 	}, function(){
 		$('.secret').fadeOut(250);
@@ -25,59 +25,18 @@ $(document).ready(function(){
 	};
 
 
-		$(window).on('scroll', function(){
-			if($(window).scrollTop() > 600){
-				$('.imgL').animate({
-					left: '80.1%',
-				});
-				$('.imgR').animate({
-					left: '145.1%',
-				});
-				// $('.imgR').css({
-				// 	'margin': '0 auto',
-				// });
-			};
-		});
-
-	
-	$('#newInputForm').on('submit', function(e){
-	//when the submit button on the form is clicked, do this
-		e.preventDefault();
-		var now = new Date();
-		var year = now.getFullYear();
-		var month = now.getMonth() + 1;
-		var day = now.getDate();
-		var timestamp = month + "/" + day + "/" + year
-		//prevents the default function of the submit button from firing
-		//doesn't send the input away
-		if ($('#newInput').val().length >= 1){
-				$('#tdul').append('<li class="li"><i class="fa fa-times-circle"></i><p>' + $("#newInput").val() + ' <span>' + timestamp + '</span></p></li>');
-			//this adds an item to the list with the same formatting as the ones that are already there
-			};
-		$('#newInput').val("");
-		//this just clears the previous input from the text box
-
-		$('.fa').on('click', function(){
-		//selects the class .fa on click, runs a function
-			$(this).parent().fadeOut(200 ,function(){
-			//fades out the element with the .fa class
-				$(this).remove();
-				//removes the element completely from the html
-			});
-		});	
-	});
-	$('#tdul').on('click', '.li', function(){
-	//selects the ul element on click, runs a function
-		$(this).toggleClass('complete');
-	}); //toggles the class complete
-
-	$('.fa').on('click', function(){
-	//selects the class .fa on click, runs a function
-		$(this).parent().fadeOut(200 ,function(){
-		//fades out the element with the .fa class
-			$(this).remove();
-			//removes the element completely from the html
-		});
-	});
+		// $(window).on('scroll', function(){
+		// 	if($(window).scrollTop() > 600){
+		// 		$('.imgL').animate({
+		// 			left: '80.1%',
+		// 		});
+		// 		$('.imgR').animate({
+		// 			left: '145.1%',
+		// 		});
+		// 		// $('.imgR').css({
+		// 		// 	'margin': '0 auto',
+		// 		// });
+		// 	};
+		// });
 
 });
