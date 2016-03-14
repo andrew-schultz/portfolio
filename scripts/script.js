a7385d980e37a161a96cfda30981a75909487c7f
Andrew Schultz <as173171@gmail.com>
2016-03-14 17:43:51 -0400

    added a project, cleaned up the code a bit, made site more responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,5 @@
 $(document).ready(function(){
 
-	$('.workBubble').hover(function(){
-		$(this).children().fadeIn(250).stop;
-	}, function(){
-		$('.secret').fadeOut(250);
-	});
-
-	$('.aboutImg2').hover(function(){
-		$(this).children().fadeIn(250).stop;
-	}, function(){
-		$('.secret').fadeOut(250);
-	});
-
-	$('#drop').hover(function(){
-		$('#dm').slideDown(300).stop;
-		}, function(){
-			$('#dm').slideUp(300).stop;
-	});
-
-	var firstanimation = function(){
-		$('.aboutImg').animate({
-			left: '300px',
-		});
-	};
-
-	$('.workBubble').hover(function(){
-		$('this:nth-child(0)').hide(),
-		$('this:nth-child(1)').show();
-	}, function(){
-		$('this:nth-child(1)').hide(),	
-		$('this:nth-child(0)').show();
-	});
 
 
 		// $(window).on('scroll', function(){
